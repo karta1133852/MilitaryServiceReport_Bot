af95618f470db887c95080df896ee70716895278
HayateShokaku <tr30111@gmail.com>
2021-12-25 22:09:39 +0800

    Finish text report

--- report.js
+++ report.js
@@ -9,6 +9,10 @@ const totalReport = require('./reportFormat/total.json');
 const personReport = require('./reportFormat/person.json');
 const singleLine = require('./reportFormat/singleLine.json');
 
+const SHEET_ID = process.env.SHEET_ID;
+const CELL_RANGE = 'A106:A118'
+const MIN_NUMBER = 106, MAX_NUMBER = 118;
+
 // create LINE SDK config from env variables
 const config = {
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
@@ -54,12 +58,8 @@ app.listen(port, () => {
 });*/
 
 async function test() {
-  const sheet_id = process.env.SHEET_ID;
-  const CELL_RANGE = 'A106:A118'
-  const MIN_NUMBER = 106, MAX_NUMBER = 118;
-
   try {
-    const doc = new GoogleSpreadsheet(sheet_id);
+    const doc = new GoogleSpreadsheet(SHEET_ID);
     await doc.useServiceAccountAuth({
       client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
       private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
@@ -71,36 +71,15 @@ async function test() {
 
     await sheet.loadCells(CELL_RANGE);
 
-    const reportTotalMessage = JSON.parse(JSON.stringify(totalReport));
-    for (let i = 110; i <= 115; i++) {
-      const newPerson = JSON.parse(JSON.stringify(personReport));
-      let message = sheet.getCellByA1('A' + i).value;
-      if (message === null) {
-        newPerson.text = '' + i;
-        newPerson.color = '#FF0000';
-        const emptyLine = JSON.parse(JSON.stringify(singleLine));
-        emptyLine.text = ' ';
-        reportTotalMessage.body.contents = reportTotalMessage.body.contents.concat([newPerson, emptyLine, emptyLine]);
-      } else {
-        let splitedLines = message.trim().split(/\s*[\r\n]+\s*/g);
-        newPerson.text = splitedLines.shift();
-        reportTotalMessage.body.contents.push(newPerson);
-        splitedLines.forEach(m => {
-          if (m !== null) {
-            const newLine = JSON.parse(JSON.stringify(singleLine));
-            newLine.text = (m === '') ? ' ' : m;
-            reportTotalMessage.body.contents.push(newLine);
-          }
-        });
-      }
-      
-    }
+    //const reportTotalMessage = writeFlexMessage(sheet);
+    const reportTotalMessage = writeTextMessage(sheet);
+    
     
     //console.log(totalReport.body.contents[3]);
     //await sheet.saveUpdatedCells();
 
     
-    console.log(JSON.stringify(reportTotalMessage));
+    //console.log(JSON.stringify(reportTotalMessage));
     // create LINE SDK client
     const client = new line.Client(config);
     client.pushMessage(process.env.GROUP_ID, reportTotalMessage)
@@ -117,6 +96,56 @@ async function test() {
   
 }
 
+function writeFlexMessage(sheet) {
+  const reportTotalMessage = JSON.parse(JSON.stringify(totalReport));
+  for (let i = MIN_NUMBER; i <= MAX_NUMBER; i++) {
+    const newPerson = JSON.parse(JSON.stringify(personReport));
+    let message = sheet.getCellByA1('A' + i).value;
+    if (message === null) {
+      newPerson.text = '' + i;
+      newPerson.color = '#FF0000';
+      const emptyLine = JSON.parse(JSON.stringify(singleLine));
+      emptyLine.text = ' ';
+      reportTotalMessage.contents.body.contents = reportTotalMessage.contents.body.contents.concat([newPerson, emptyLine, emptyLine]);
+    } else {
+      let splitedLines = message.trim().split(/\s*[\r\n]+\s*/g);
+      newPerson.text = splitedLines.shift();
+      reportTotalMessage.contents.body.contents.push(newPerson);
+      splitedLines.forEach(m => {
+        if (m !== null) {
+          const newLine = JSON.parse(JSON.stringify(singleLine));
+          newLine.text = (m === '') ? ' ' : m.trim();
+          reportTotalMessage.contents.body.contents.push(newLine);
+        }
+      });
+    }
+  }
+
+  return reportTotalMessage;
+}
+
+function writeTextMessage(sheet) {
+  let reportMessage = '';
+  for (let i = MIN_NUMBER; i <= MAX_NUMBER; i++) {
+    let message = sheet.getCellByA1('A' + i).value;
+    if (message === null) {
+      reportMessage += i + '\n\n';
+    } else {
+      let splitedLines = message.trim().split(/\s*[\r\n]+\s*/g);
+      reportMessage += splitedLines.shift().trim() + '\n';
+      splitedLines.forEach(m => {
+        if (m !== null) {
+          reportMessage += '　' + m.trim() + '\n';
+        }
+      });
+    }
+  }
+
+  return {
+    type: 'text',
+    text: reportMessage
+  };
+}
 
 // 執行
 (async function() {
