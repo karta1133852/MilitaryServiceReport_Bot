399bf54c5ca25a52a5382850ead8cc6ebc2e3401
HayateShokaku <tr30111@gmail.com>
2022-01-16 02:10:44 +0800

    test

--- utils/dropTables.js
+++ utils/dropTables.js
@@ -5,8 +5,12 @@ const sqlDropTable = 'DROP TABLE report_content; ' +
   'DROP TABLE report_date;';
 
 (async () => {
-  const res = await pgQuery(sqlDropTable).catch(err => console.log(err));
-  if (res !== null) {
-    console.log('成功刪除資料表');
+  try {
+    const res = await pgQuery(sqlDropTable);
+    if (res !== null) {
+      console.log('成功刪除資料表');
+    } 
+  } catch (err) {
+    
   }
 })();
